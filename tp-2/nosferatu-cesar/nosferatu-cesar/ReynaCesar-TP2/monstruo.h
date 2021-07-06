69d8885268e0de903de1c9085e2e01931ab344ce
alejandrodelcs <alejodc15@gmail.com>
2021-07-06 20:11:06 -0300

    tp-2

---
+++ tp-2/nosferatu-cesar/nosferatu-cesar/ReynaCesar-TP2/monstruo.h
@@ -0,0 +1,9 @@
+#ifndef TP2_MONSTRUO_H
+#define TP2_MONSTRUO_H
+#include "ser.h"
+class monstruo : public ser{
+public:
+};
+
+
+#endif //TP2_MONSTRUO_H
---
+++ tp-2/nosferatu-cesar/nosferatu-cesar/ReynaCesar-TP2/ser.h
@@ -0,0 +1,10 @@
+#ifndef TP2_SER_H
+#define TP2_SER_H
+#include "objeto.h"
+
+class ser : public objeto{
+protected:
+public:
+};
+
+#endif //TP2_SER_H
