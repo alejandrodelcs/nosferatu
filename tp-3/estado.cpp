1cc47143e964110926d08c8d9ee8699da8aa7161
alejandrodelcs <alejodc15@gmail.com>
2021-07-24 19:27:45 -0300

    cambios en la lectura del archivo

--- tp-3/estado.cpp
+++ tp-3/estado.cpp
@@ -1,36 +1,90 @@
 #include "estado.h"
 
-Estado::Estado(const std::string &lineaArchivo) {
-    this->lineaArchivo = lineaArchivo;
-    this->nombreObjeto = lineaArchivo.substr(0, int(this->lineaArchivo.find(' ')));
+Estado::Estado() {
+    this->cantidad = 0;
+    this->lineaArchivo = "";
+    this->identificador = "";
+    this->posicionX = 0;
+    this->posicionY = 0;
+}
+
+void Estado::nombreObjeto() {
+    this->nombre = lineaArchivo.substr(0, int(this->lineaArchivo.find(' ')));
+}
+
+void Estado::coordenadaObjeto() {
     this->coordenada = lineaArchivo.substr(int(this->lineaArchivo.find(' ')) + 1);
 }
 
 
-std::string Estado::devolverNombre() {
-    return (int(this->coordenada.find("CV")) != -1) ? "CV" : this->nombreObjeto;
+void Estado::leerLineaArchivo(std::string linea) {
+    this->lineaArchivo = linea;
+    nombreObjeto();
+    coordenadaObjeto();
+
 }
 
-int Estado::devolverCantidad() {
-    return stoi((coordenada[0] != '(' && int(this->coordenada.find("CV")) == -1) ?
-                this->coordenada.substr(0, this->coordenada.find(' ')) : "0");
+void Estado::validarNombreObjeto(){
+    if (int(this->coordenada.find("CV")) != -1)
+        this->nombre="CV";
+}
+
+std::string Estado::devolverNombreObjeto() {
+    validarNombreObjeto();
+    return this->nombre;
+}
+
+
+void Estado::validarCantidadObjeto() {
+    if ((coordenada[0] != '(') && (int(this->coordenada.find("CV")) == -1))
+        this->cantidad = stoi(this->coordenada.substr(0, this->coordenada.find(' ')));
+    else
+        this->cantidad = 0; //si el objeto no tene cantidad devuelve 0
+}
+
+int Estado::devolverCantidad(){
+
+    validarCantidadObjeto();
+    return this->cantidad;
+}
+
+void Estado::validarPosicionX(){
+    if ((int(this->coordenada.find("CV")) != -1 || int(NUMEROS.find(this->coordenada[0])) != -1)
+    && (int(NUMEROS.find(this->nombre[0])) == -1)){
+        this->posicionX = stoi(this->coordenada.substr(int(coordenada.find('(')) + 1,
+                                                       int(coordenada.find(',')) - int(coordenada.find('(')) - 1));
+    }else{
+        this->posicionX = stoi(coordenada.substr(1, int(coordenada.find(',')) - 1));
+    }
 }
 
 int Estado::devolverPosx() {
-    return stoi((int(this->coordenada.find("CV")) != -1
-                 || int(NUMEROS.find(this->coordenada[0])) != -1)
-                && (int(NUMEROS.find(this->nombreObjeto[0])) == -1)
-                ? this->coordenada.substr(int(coordenada.find('(')) + 1,
-                                          int(coordenada.find(',')) - int(coordenada.find('(')) - 1)
-                : coordenada.substr(1, int(coordenada.find(',')) - 1)) - 1;
+    validarPosicionX();
+    return this->posicionX-1;
+}
+
+void Estado::validarPosicionY() {
+    this->posicionY = stoi(coordenada.substr(int(coordenada.find(',')) + 2, int(coordenada.find(')')) - 2));
 }
 
 int Estado::devolverPosy() {
-    return stoi(coordenada.substr(int(coordenada.find(',')) + 2, int(coordenada.find(')')) - 2)) - 1;
+    validarPosicionY();
+    return this->posicionY-1;
+}
+
+
+void Estado::validarIdentificador() {
+    if (int(lineaArchivo.find(')')) != -1){
+        this->identificador = lineaArchivo.substr(lineaArchivo.find(')')+2);
+    }
 }
 
 std::string Estado::devolverId() {
-    return (int(lineaArchivo.find(')')) != -1) ? lineaArchivo.substr(lineaArchivo.find(')')+2) : "";
+    validarIdentificador();
+   return this->identificador;
 }
+
+
+
 
 #include "estado.h"
